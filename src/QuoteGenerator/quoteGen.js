3db0c0b7f92be3586b60b87b179a2bfa875df32a
dpshah269 <dpshah269@gmail.com>
2023-07-28 19:35:16 +0530

    Quote Generator updated

--- src/QuoteGenerator/quoteGen.js
+++ src/QuoteGenerator/quoteGen.js
@@ -1,40 +1,65 @@
 import { useState, useEffect, Fragment } from "react";
+import './quoteGen.css'
+import { FaRandom} from 'react-icons/fa';
+import { BiSolidQuoteAltLeft,BiSolidQuoteAltRight } from 'react-icons/bi';
+
 export default function QuoteApp()
 {
     const [quote,setQuote] = useState([])
     const [data,setData] = useState()
     // const [count,setCount] = useState(0)
-    
+    // const RandomCount = () =>{
+    //     // console.log(data,Math.floor(Math.random() * (data.length)))
+    //     if(data){
+    //         const count = Math.floor(Math.random() * (data.length));
+    //     // console.log(count)
+    //         console.log(quote)
+    //         setQuote([{text:data[count].text,author:data[count].author}])
+    //     }
+    // }
+
+    // const count = data && Math.floor(Math.random() * (data.length));
+    const randomData = (data) =>{
+        // setData(data)
+        const count = data && Math.floor(Math.random() * (data.length));
+        setQuote([{text:data[count].text,author:data[count].author}])
+    }
+
     useEffect(()=>{
         fetch('https://type.fit/api/quotes')
         .then(response=>{
         response.json()
-        .then(data=> {
-            setData(data)
-            setQuote([{text:data[0].text,author:data[0].author}])
+        .then(json=> {
+            setData(json)
+            randomData(json)
+
+            // setQuote([{text:data[0].text,author:data[0].author}])
             // setQuote(data[count].text);
-        });
         })
+        
+        })
+        
         .catch(error=> {
         console.log('Fetch Error:', error);
         });
     },[])
     return <Fragment>
-        <h1>Random Quotes</h1>
-        {quote.length > 0 && (
-            <blockquote>
-                <div>{quote[0].text}</div>
-                <div>{quote[0].author}</div>
-            </blockquote>
-        )}
-        <button onClick={()=>{
-            // console.log(data,Math.floor(Math.random() * (data.length)))
-            if(data){
-                const count = Math.floor(Math.random() * (data.length));
-            // console.log(count)
-                console.log(quote)
-                setQuote([{text:data[count].text,author:data[count].author}])
-            }
-        }}>New Quote</button>
+        <div className="quoteMainDiv">
+            <div>
+                <h1>Random Quotes</h1>
+                <br/>
+                {quote.length > 0 && (
+                    <div className="quoteSubDiv">
+                        <div><BiSolidQuoteAltLeft/>{quote[0].text}<BiSolidQuoteAltRight/></div>
+                        <div className="blockquote-footer">{(quote[0].author).split(',')[0]}</div>
+                    </div>
+                )}
+                <button onClick={()=>{randomData(data)}}>
+                    <FaRandom/>
+                </button>
+            </div>
+        </div>
+        
+        
     </Fragment>
 }

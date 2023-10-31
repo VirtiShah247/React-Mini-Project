c8600a277cc9bbc570303ef6e8aaeb52d0d3d017
Virti Shah <vpshah247@gmail.com>
2023-11-01 03:40:07 +0530

    Update carouseIndex.js

--- src/carousel/carouselIndex.js
+++ src/carousel/carouselIndex.js
@@ -3,30 +3,31 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import './carousel.css'
 export default function CarouselApp()
 {
-    const imageObj = [
-        {
-            id:0,
-            url:"https://th.bing.com/th/id/OIP.1YM53mG10H_U25iPjop83QHaEo?pid=ImgDet&rs=1"
-        },
-        {
-            id:1,
-            url:"https://th.bing.com/th/id/R.7f34a70a5bee79488309934920d316cb?rik=IRJI6pSFo6OI%2fQ&riu=http%3a%2f%2fallpicts.in%2fwp-content%2fuploads%2f2018%2f03%2fNatural-Images-HD-1080p-Download-with-Keyhole-Arch-at-Pfeiffer-Beach.jpg&ehk=JPI0MFI04N62Xtd7MT1P2sw4vJgwODLsp1EcpgvuGFo%3d&risl=&pid=ImgRaw&r=0"
-        },
-        {
-            id:2,
-            url:"https://www.bwallpaperhd.com/wp-content/uploads/2021/01/NashPoint.jpg"
-        },
-        {
-            id:3,
-            url:"https://th.bing.com/th/id/R.56dde41aee17e96425987f751a9269ce?rik=PimP3aKlBih1aA&riu=http%3a%2f%2fwww.hdwallpaper.nu%2fwp-content%2fuploads%2f2015%2f09%2ftropical_beach_blue_summer_sea_emerald_sand_hd-wallpaper-1701606.jpg&ehk=OVN5KYNNiPQtsbvJxJZF3%2fZMhpDEpVc4ebUXS5y20%2f4%3d&risl=&pid=ImgRaw&r=0"
-        }
-    ]
-    const [image,setImage] = useState(imageObj[0].url);
-    const [count,setCount] = useState(0)
-
+    const [count,setCount] = useState(0);
     useEffect(()=>{
+        const imageObj = [
+            {
+                id:0,
+                url:"https://th.bing.com/th/id/OIP.1YM53mG10H_U25iPjop83QHaEo?pid=ImgDet&rs=1"
+            },
+            {
+                id:1,
+                url:"https://th.bing.com/th/id/R.7f34a70a5bee79488309934920d316cb?rik=IRJI6pSFo6OI%2fQ&riu=http%3a%2f%2fallpicts.in%2fwp-content%2fuploads%2f2018%2f03%2fNatural-Images-HD-1080p-Download-with-Keyhole-Arch-at-Pfeiffer-Beach.jpg&ehk=JPI0MFI04N62Xtd7MT1P2sw4vJgwODLsp1EcpgvuGFo%3d&risl=&pid=ImgRaw&r=0"
+            },
+            {
+                id:2,
+                url:"https://www.bwallpaperhd.com/wp-content/uploads/2021/01/NashPoint.jpg"
+            },
+            {
+                id:3,
+                url:"https://th.bing.com/th/id/R.56dde41aee17e96425987f751a9269ce?rik=PimP3aKlBih1aA&riu=http%3a%2f%2fwww.hdwallpaper.nu%2fwp-content%2fuploads%2f2015%2f09%2ftropical_beach_blue_summer_sea_emerald_sand_hd-wallpaper-1701606.jpg&ehk=OVN5KYNNiPQtsbvJxJZF3%2fZMhpDEpVc4ebUXS5y20%2f4%3d&risl=&pid=ImgRaw&r=0"
+            }
+        ]
+        // setInitial(imageObj[0].url)
         setImage(imageObj[count].url)
-    },[])
+    },[count])
+    // const [intial, setInitial] = useState();
+    const [image,setImage] = useState();
     return <Fragment>
         <div className='container carouselMainDiv'>
             <h3 id="mainHeading">Carousel</h3>
